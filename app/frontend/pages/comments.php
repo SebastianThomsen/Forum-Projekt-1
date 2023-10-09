100d7e8290778b300521ef9cca53d1b10ac7e0a3
Sebastian Thomsen <seba9323@mail.hansenberg.dk>
2023-10-09 09:09:22 +0200

    Create comment

--- app/frontend/pages/comments.php
+++ app/frontend/pages/comments.php
@@ -5,9 +5,16 @@
             <h2>The Comment Section</h1>
 
                 <?php
-
-                if ($comments->count()) {
+                echo '<a href="create-comment.php?post_id=' . $post_id . '" class="btn btn-primary">Create a comment</a>';
+                if (isset($comments) && $comments->count()) {
                     foreach ($comments->results() as $c) {
+                        echo '<div class="card">';
+                        echo '<div class="card-body">';
+                        echo '<h4 class="card-title">' . $c->user_id . '</h4>';
+                        echo '<p class="card-text">' . $c->content . '</p>';
+                        echo '<p class="card-text">' . $c->created_at . '</p>';
+                        echo '</div>';
+                        echo '</div>';
                     }
                 } else {
                     echo '<div class="alert alert-danger"><strong></strong>No comments found!</div>';
